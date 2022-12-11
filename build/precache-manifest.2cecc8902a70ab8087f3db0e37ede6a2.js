52e1ff3cc4b5fe62dd6bb445d787896aafbdb0d3
Ilan Elfen <u24ielfen@wbais.net>
2022-12-11 21:23:01 +0200

    big things

---
+++ build/precache-manifest.2cecc8902a70ab8087f3db0e37ede6a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1f83a99c18612a9210",
+    "url": "/static/css/main.25c34cc6.chunk.css"
+  },
+  {
+    "revision": "3e1f83a99c18612a9210",
+    "url": "/static/js/main.3e1f83a9.chunk.js"
+  },
+  {
+    "revision": "27e674cd7bcf1992b90b",
+    "url": "/static/js/1.27e674cd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb4965343fa2884f29aeb1df2f338d5a",
+    "url": "/index.html"
+  }
+];
